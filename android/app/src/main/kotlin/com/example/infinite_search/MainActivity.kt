5d340740ef3c1a312e2726ea30dc93ed4ea63449
Alba <alba__22@outlook.com>
2024-05-30 13:18:57 -0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/infinite_search/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.infinite_search
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/app/app_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
